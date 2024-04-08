214baa7389003cd24ab42cab70d1f394ae4f37be
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-04-07 21:18:21 -0400

    changed encoding

--- process_data.py
+++ process_data.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 def main():
-    df = pd.read_csv('./movies/MovieGenre.csv', encoding='utf-8')
+    df = pd.read_csv('./movies/MovieGenre.csv', encoding='ISO-8859-1')
     df.dropna(inplace=True)
     df.drop_duplicates(subset=['imdbId'], inplace=True)
-
+    
 if __name__ == "__main__":
     main()
