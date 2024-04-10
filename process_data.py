d704bc2c1e29c68a6454ae45485d65b5046c02eb
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-04-10 18:38:26 -0400

    wrote get_image function to get pixels from image link

--- process_data.py
+++ process_data.py
@@ -1,19 +1,24 @@
 import pandas as pd
 import requests
 import shutil
+from PIL import Image
+import numpy as np
+import os
 
 def get_image(link, imdbId):
     res = requests.get(link, stream=True)
     if res.status_code == 404:
         return None
-    with open('./movies/posters/' + str(imdbId) + '.png', 'wb') as f:
+    with open('./movies/' + str(imdbId) + '.png', 'wb') as f:
         shutil.copyfileobj(res.raw, f)
+    image = np.asarray(Image.open('./movies/' + str(imdbId) + '.png'))
+    os.remove('./movies/' + str(imdbId) + '.png')
+    return image
 
 def main():
     df = pd.read_csv('./movies/MovieGenre.csv', encoding='ISO-8859-1')
     df = df.dropna()
     df = df.drop_duplicates(subset=['imdbId'])
-    df = df.apply(lambda x : x)
     print(len(df))
     print(df)
     #throws a KeyError when i == 137, not sure why
